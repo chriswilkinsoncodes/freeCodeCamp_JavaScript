ba715820335dc236404d93247eca816206b1b7b8
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-12-03 13:05:00 -0500

    initial commit

---
+++ falsy_bouncer.js
@@ -0,0 +1,16 @@
+function bouncer(arr) {
+    let arrTrue = []
+    for (let i = 0; i < arr.length; i++) {
+      if (Boolean(arr[i]) == true) {
+        arrTrue.push(arr[i])
+  
+      }
+  
+    }
+    return arrTrue;
+  }
+  
+console.log(bouncer([7, "ate", "", false, 9]))
+console.log(bouncer(["a", "b", "c"]))
+console.log(bouncer([false, null, 0, NaN, undefined, ""]))
+console.log(bouncer([null, NaN, 1, 2, undefined]))
