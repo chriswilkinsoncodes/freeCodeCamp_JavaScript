c6547f640a4aec5558bcd45a42a2bede4ac6eb32
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-12-06 13:35:54 -0500

    included arr in output

--- mutations.js
+++ mutations.js
@@ -1,10 +1,10 @@
 function mutation(arr) {
     for (let i = 0; i < arr[1].length; i++) {
         if (!arr[0].toLowerCase().includes(arr[1][i].toLowerCase())) {
-          return false
+          return [arr, false]
         }
     }
-    return true;
+    return [arr, true];
   }
   
 console.log(mutation(["hello", "hey"]));
