1d3e363315bd1a8efb4fd316d1d7318925268a1c
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-12-12 17:12:09 -0500

    initial commit

---
+++ seek_destroy.js
@@ -0,0 +1,18 @@
+function destroyer(arr, ...x) {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+      if (x.indexOf(arr[i]) < 0) {
+        result.push(arr[i])
+      }
+    }
+    return result;
+  }
+
+
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5));
+console.log(destroyer(["tree", "hamburger", 53], "tree", 53));
+console.log(destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan"));
+  
